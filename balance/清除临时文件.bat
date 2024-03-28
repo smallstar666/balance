374bc0ec00be464e20c8955b1a5f48e4f578dc48
smallstar666 <2065400281@qq.com>
2024-03-28 23:07:24 +0800

    initial code

---
+++ balance/清除临时文件.bat
@@ -0,0 +1,14 @@
+del *.o /s
+del *.d /s
+del *.crf /s
+del *.axf /s
+del *.htm /s
+del *.lnp /s
+del *.sct /s
+del *.iex /s
+del *.dep /s
+del *.dbgconf /s
+del *.map /s
+del *lst /s
+del *hex /s
+exit
