624b509a08d2f49909cd9edc735673e2be4bbdeb
smallstar666 <2065400281@qq.com>
2024-03-28 23:24:58 +0800

    add led test--PC13

--- balance/USER/main.c
+++ balance/USER/main.c
@@ -1,6 +1,24 @@
 #include "stm32f10x.h"
+#include "led.h"
+
+//´ÖÂÔÑÓÊ±
+void delay(uint32_t timers)
+{
+	uint16_t i=0,j=0;
+	for(i = 0;i<timers;i++)
+	{
+		for(j =0;j<65535;j++);
+	}
+}
 
 int main(void)
 {
-	
+	LED_Init();
+	while(1)
+	{
+		LED_ON();
+		delay(100);
+		LED_OFF();
+		delay(100);
+	}
 }
