bdf29a4bcfc731022a530428e59b12ba774b3a85
smallstar666 <2065400281@qq.com>
2024-03-31 15:50:30 +0800

    add oled display

--- balance/USER/main.c
+++ balance/USER/main.c
@@ -3,6 +3,7 @@
 #include "delay.h"
 #include "usart.h"
 #include "adc.h"
+#include "OLED.h"
 #include "stdio.h"
 
 int main(void)
@@ -11,7 +12,12 @@ int main(void)
 	Delay_Init();      //延时初始化
 	Usart_Init(115200);//串口初始化
 	ADC_init();        //ADC初始化
-	printf("hello world!\r\n");
+	OLED_Init();       //OLED初始化
+	printf("Balance Car\r\n");
+	OLED_ShowString(1, 1, "Balance Car");
+	OLED_ShowString(2, 1, "---------------");
+	OLED_ShowString(3, 3, "Designed By");
+	OLED_ShowString(4, 4, "Xiao Xing");
 	while(1)
 	{
 		LED_ON();
